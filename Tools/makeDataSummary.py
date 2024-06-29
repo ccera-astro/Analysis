a8329e897118ca719002ce5a6ca1a0ea0b14e442
Daniel Marlow <marlow@princeton.edu>
2024-06-29 09:26:24 -0400

    debug info

--- Tools/makeDataSummary.py
+++ Tools/makeDataSummary.py
@@ -38,7 +38,7 @@ print("files={0:s}".format(str(files)))
 outLines = [] 
 for file in files :
     print("Reading file={0:s}".format(file))
-    line = file.split('/')[-1]
+    line = file.split('/')[-1] + ", "
     metadata = getMetaData(file)
     try : line += metadata['run_mode'] + ", "
     except KeyError : line += "Unknown, "
@@ -48,7 +48,7 @@ for file in files :
     line += "{0:d}, ".format(metadata['fft_size']) 
     line += "{0:.6f}, ".format(float(metadata['t_sample']))
     line += "{0:.1f}, ".format(1.e-6*float(metadata['freq']))
-    line += "{0:.3f}, ".format(1.e-6*float(metadata['srate']))
+    line += "{0:.3f} ".format(1.e-6*float(metadata['srate']))
     line += "\n"
     outLines.append(line)
 
